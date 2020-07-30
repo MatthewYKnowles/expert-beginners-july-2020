570e432a7ad705186598f39667aa154050fc5fd8
MatthewYKnowles <matthewyknowles@gmail.com>
2020-07-30 10:38:56 -0400

    created execise starter

--- cypress/integration/emmersion.spec.ts
+++ cypress/integration/emmersion.spec.ts
@@ -15,17 +15,22 @@ describe("Emmersion End to End Tests", () => {
         navigationBar = new NavigationBar();
     })
 
-    it.skip('not using page object: should create admin and see it in the admin list', () => {
+    it('not using page object: should create admin and see it in the admin list', () => {
+        const username = 'local-holonet';
+        const password = 'Testing1!'
+        let accountName = 'Local TN + WC-account';
+
         const randomNumber = Math.floor(Math.random() * 1000);
         const firstName = `TestFirst${randomNumber}`;
         const lastName = `TestLast${randomNumber}`;
         const email = `${firstName}@emmersion.ai`;
+
         cy.visit('/')
-        cy.get('[data-test=username]').type('local-holonet')
-        cy.get('[data-test=password]').type('Testing1!')
+        cy.get('[data-test=username]').type(username)
+        cy.get('[data-test=password]').type(password)
         cy.get('[data-test=sign-in]').click();
-        cy.get('[data-test=search]').type('Local');
-        cy.get('[data-test="Local TN + WC-account"]').click();
+        cy.get('[data-test=search]').type(accountName);
+        cy.get(`[data-test="${accountName}"]`).click();
         cy.get('#addUserGroupFormModal > .fa').click();
         cy.get('ul > :nth-child(2) > label').click();
         cy.get('#givenName').type(firstName);
@@ -41,9 +46,20 @@ describe("Emmersion End to End Tests", () => {
         cy.get('#assessmentsId').click({force: true})
         cy.get('#admin-listId').click({force: true})
         cy.contains(`${lastName}, ${firstName}`).should('be.visible');
+        cy.get('.dropdown-toggle').click();
+        cy.get('#Logout').click();
+        cy.get('#sign-in').click();
+        cy.get('[data-test=username]').type(username)
+        cy.get('[data-test=password]').type(password)
+        cy.get('[data-test=sign-in]').click();
+        cy.get('[data-test=search]').type(accountName);
+        cy.get(`[data-test="${accountName}"]`).click();
+        cy.contains(`${firstName} ${lastName}`).parent().find('.fa-trash').click()
+        cy.get('.cancel-btn').click()
+        cy.contains(`${firstName} ${lastName}`).should('not.be.visible');
     })
 
-    it('using page object: should create admin and see it in the admin list', () => {
+    it.skip('using page object: should create admin and see it in the admin list', () => {
         const username = 'local-holonet';
         const password = 'Testing1!'
         let accountName = 'Local TN + WC-account';
@@ -65,8 +81,8 @@ describe("Emmersion End to End Tests", () => {
         let accountSettingsPage: AccountSettingsPage = adminDashboard.goToAccountSettings();
         accountSettingsPage.viewAdminManagement()
         cy.contains(`${lastName}, ${firstName}`).should('be.visible');
-        let logoutpage: LogoutPage = navigationBar.logout();
-        loginPage = logoutpage.goToLoginPage();
+        let logoutPage: LogoutPage = navigationBar.logout();
+        loginPage = logoutPage.goToLoginPage();
         loginPage.loginToHolonet(username, password);
         accountSummaryPage = holonetDashboardPage.goToAccountSummary(accountName)
         accountSummaryPage.deleteAdmin(`${firstName} ${lastName}`);
--- cypress/integration/exercise.spec.ts
+++ cypress/integration/exercise.spec.ts
@@ -1,5 +1,17 @@
+import {LoginPage} from "../support/pages/login.page";
+import {HolonetDashboardPage} from "../support/pages/holonet-dashboard.page";
+
 describe("", () => {
-    it("should execute a test", ()=> {
-        cy.visit("/");
+    it("should create a new account, create admin on account, and verify admin shows up on new admin list - brody", ()=> {
+        const username = 'local-holonet';
+        const password = 'Testing1!'
+        const randomNumber = Math.floor(Math.random() * 1000);
+        cy.visit('/')
+        let loginPage = new LoginPage();
+        let holonetDashboardPage: HolonetDashboardPage = loginPage.loginToHolonet(username, password);
+        cy.get('#showCreateModal').click();
+        cy.viewport(1920, 1080);
+        cy.get('#accountName').type(`test${randomNumber}`)
+        cy.get('#membershipLevelId').select('Standard')
     });
 })
